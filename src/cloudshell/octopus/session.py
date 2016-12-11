2cde3252635fbc11e1d576cd84f1ef7ba7568820
Nahum Timerman <ntimerman@gmail.com>
2016-12-11 18:14:42 +0200

    deploy environment + test for deploy env

--- src/cloudshell/octopus/session.py
+++ src/cloudshell/octopus/session.py
@@ -1,2 +1,65 @@
+from cloudshell.octopus.environment import Environment
+import requests
+import json
+from urlparse import urljoin
+
+
 class OctopusServer:
-    pass
+    def __init__(self, host, api_key):
+        """
+        :param host:
+        :param api_key:
+        :return:
+        """
+        self._host = host
+        self._api_key = api_key
+        self._validate_host()
+
+    def _validate_host(self):
+        result = requests.get(self.host)
+        self._valid_status_code(result, 'Could not reach {0}\nPlease check if server is accessible'
+                                .format(self._host))
+
+
+    @property
+    def host(self):
+        return self._host
+
+    @property
+    def api_key(self):
+        return self._api_key
+
+    def deploy_environment(self, environment):
+        """
+        :type environment: cloudshell.octopus.environment.Environment
+        :return:
+        """
+        api_url = urljoin(self.host, '/api/environments')
+        result = requests.post(api_url, params={'ApiKey': self._api_key}, json=environment.json)
+        self._valid_status_code(result, 'Failed to deploy environment; error: {0}'.format(result.text))
+        environment.set_id(json.loads(result.content)['Id'])
+
+    def environment_exists(self, environment):
+        if hasattr(environment, 'id'):
+            api_url = urljoin(self.host, '/api/environments/{0}'.format(environment.id))
+            result = requests.get(api_url, params={'ApiKey': self._api_key}, json=environment.json)
+            try:
+                self._valid_status_code(result, 'Environment not found; error: {0}'.format(result.text))
+            except:
+                return False
+            else:
+                return True
+        else:
+            # Environment has no id, maybe you haven't deployed it yet?
+            return False
+
+    def delete_environment(self, environment):
+        if not self.environment_exists(environment):
+            raise Exception('Environment does not exist')
+        api_url = urljoin(self.host, '/api/environments/{0}'.format(environment.id))
+        result = requests.delete(api_url, params={'ApiKey': self._api_key}, json=environment.json)
+
+    def _valid_status_code(self, result, error_msg):
+        # Consider any status other than 2xx an error
+        if not result.status_code // 100 == 2:
+            raise Exception(error_msg)
--- src/tests/test_environment.py
+++ src/tests/test_environment.py
@@ -1,16 +1,19 @@
 import unittest
 from cloudshell.octopus.session import OctopusServer
 from cloudshell.octopus.environment import Environment
+from context import OCTOPUS_HOST, OCTOPUS_API_KEY
 
 
 class EnvironmentTest(unittest.TestCase):
     def test_create_new_environment(self):
-        octo_session = OctopusServer()
-        quali_env = Environment(name='a short environment name, preferably 5-20 chars',
+        octo_session = OctopusServer(host=OCTOPUS_HOST,
+                                     api_key=OCTOPUS_API_KEY)
+        quali_env = Environment(name='some_appropriate_name',
                                 description='Some environment lol who carez yo!',
-                                sort_order='5',
-                                # PUT	If set to true, deployments will prompt for manual intervention (Fail/Retry/Ignore) when failures are encountered in activities that support it. May be overridden with the Octopus.UseGuidedFailure special variable.
-                                user_guided_failure=False)
+                                sort_order=5,
+                                use_guided_failure=False)
         # so you can deploy to different octopus servers?
-        quali_env.deploy(octo_session)
+        octo_session.deploy_environment(quali_env)
         self.assertTrue(octo_session.environment_exists(quali_env))
+        octo_session.delete_environment(quali_env)
+
