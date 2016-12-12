3e030fad2c73cee5fc8979800db033abf8125029
Nahum Timerman <ntimerman@gmail.com>
2016-12-12 18:32:02 +0200

    deploy release to environemnt

--- src/cloudshell/octopus/session.py
+++ src/cloudshell/octopus/session.py
@@ -3,6 +3,7 @@ import requests
 import json
 from urlparse import urljoin
 import urllib
+import time
 
 import ssl
 VALIDATE_TENTACLE_CONTEXT = ssl._create_unverified_context()
@@ -40,21 +41,61 @@ class OctopusServer:
         """
         api_url = urljoin(self.host, '/api/environments')
         result = requests.post(api_url, params={'ApiKey': self._api_key}, json=environment_spec.json)
-        self._valid_status_code(result, 'Failed to deploy environment_spec; error: {0}'.format(result.text))
+        self._valid_status_code(result, 'Failed to deploy environment; error: {0}'.format(result.text))
         environment_spec.set_id(json.loads(result.content)['Id'])
         return environment_spec
 
     def create_machine(self, machine_spec):
         """
-        :type environment_spec: cloudshell.octopus.environment_spec.EnvironmentSpec
+        :type machine_spec: cloudshell.octopus.machine_spec.MachineSpec
         :return:
         """
         self._validate_tentacle_uri(machine_spec.uri)
         api_url = urljoin(self.host, '/api/machines')
         result = requests.post(api_url, params={'ApiKey': self._api_key}, json=machine_spec.json)
-        self._valid_status_code(result, 'Failed to create machine_spec; error: {0}'.format(result.text))
+        self._valid_status_code(result, 'Failed to create machine; error: {0}'.format(result.text))
         machine_spec.set_id(json.loads(result.content)['Id'])
         return machine_spec
+
+    def create_release(self, release_spec):
+        """
+        :type release_spec: cloudshell.octopus.release_spec.ReleaseSpec
+        :return:
+        """
+        api_url = urljoin(self.host, '/api/releases')
+        result = requests.post(api_url, params={'ApiKey': self._api_key}, json=release_spec.json)
+        self._valid_status_code(result, 'Failed to create release; error: {0}'.format(result.text))
+        release_spec.set_id(json.loads(result.content)['Id'])
+        return release_spec
+
+    def deploy_release(self, release_spec, environment_spec):
+        """
+        :param release_spec: cloudshell.octopus.release_spec.ReleaseSpec
+        :type environment_spec: cloudshell.octopus.environment_spec.EnvironmentSpec
+        :return:
+        """
+        api_url = urljoin(self.host, '/api/deployments')
+        deployment = {
+            'EnvironmentId': environment_spec.id,
+            'ReleaseId': release_spec.id
+        }
+        result = requests.post(api_url, params={'ApiKey': self._api_key}, json=deployment)
+        self._valid_status_code(result, 'Failed to deploy release; error: {0}'.format(result.text))
+        deployment_result = json.loads(result.text)
+        return deployment_result
+
+    def wait_for_deployment_to_complete(self, deployment_result, retries=5, sleep_seconds=60):
+        deployment_outcome = False
+        api_url = urljoin(self.host, 'api/tasks/{0}'.format(deployment_result['TaskId']))
+        for retry in range(retries):
+            try:
+                result = requests.get(api_url, params={'ApiKey': self._api_key})
+                self._valid_status_code(result, '')
+                deployment_outcome = True
+                break
+            except:
+                time.sleep(sleep_seconds)
+        return deployment_outcome
 
     def environment_exists(self, environment):
         if hasattr(environment, 'id'):
@@ -84,6 +125,20 @@ class OctopusServer:
             # EnvironmentSpec has no id, maybe you haven't deployed it yet?
             return False
 
+    def release_exists(self, release_spec):
+        if hasattr(release_spec, 'id'):
+            api_url = urljoin(self.host, '/api/releases/{0}'.format(release_spec.id))
+            result = requests.get(api_url, params={'ApiKey': self._api_key}, json=release_spec.json)
+            try:
+                self._valid_status_code(result, 'Machine not found; error: {0}'.format(result.text))
+            except:
+                return False
+            else:
+                return True
+        else:
+            # EnvironmentSpec has no id, maybe you haven't deployed it yet?
+            return False
+
     def delete_environment(self, environment):
         if not self.environment_exists(environment):
             raise Exception('Environment does not exist')
@@ -94,6 +149,12 @@ class OctopusServer:
         if not self.machine_exists(machine_spec):
             raise Exception('Machine does not exist')
         api_url = urljoin(self.host, '/api/machines/{0}'.format(machine_spec.id))
+        result = requests.delete(api_url, params={'ApiKey': self._api_key})
+
+    def delete_release(self, release_spec):
+        if not self.release_exists(release_spec):
+            raise Exception('Release does not exist')
+        api_url = urljoin(self.host, '/api/releases/{0}'.format(release_spec.id))
         result = requests.delete(api_url, params={'ApiKey': self._api_key})
 
     def _valid_status_code(self, result, error_msg):
--- src/tests/test_environment.py
+++ src/tests/test_environment.py
@@ -2,7 +2,9 @@ import unittest
 from cloudshell.octopus.session import OctopusServer
 from cloudshell.octopus.environment_spec import EnvironmentSpec
 from cloudshell.octopus.machine_spec import MachineSpec
-from context import OCTOPUS_HOST, OCTOPUS_API_KEY, TENTACLE_SERVER
+from cloudshell.octopus.release_spec import ReleaseSpec
+from context import OCTOPUS_HOST, OCTOPUS_API_KEY, TENTACLE_SERVER, THUMBPRINT, PROJECT_ID
+from random import randint
 
 
 class OctopusDeployTest(unittest.TestCase):
@@ -12,29 +14,71 @@ class OctopusDeployTest(unittest.TestCase):
                                               description='Some environment lol who carez yo!',
                                               sort_order=5,
                                               use_guided_failure=False)
+        self.QUALI_RELEASE_SPEC = ReleaseSpec(project_id=PROJECT_ID,
+                                              version='.'.join([str(randint(0, 8000000)),
+                                                                str(randint(0, 8000000)),
+                                                                str(randint(0, 8000000))]),
+                                              release_notes='Ya welli')
+        self.MACHINE_SPEC = MachineSpec(name='machine_name',
+                                        roles=['role1', 'role2'],
+                                        thumbprint=THUMBPRINT,
+                                        uri=TENTACLE_SERVER,
+                                        environment_ids=[])
 
     def tearDown(self):
-        if hasattr(self, 'MACHINE'):
-            self.OCTO_SESSION.delete_machine(self.MACHINE)
-        self.OCTO_SESSION.delete_environment(self.QUALI_ENV_SPEC)
+        if hasattr(self, 'MACHINES'):
+            for machine in self.MACHINES:
+                self.OCTO_SESSION.delete_machine(machine)
+        if hasattr(self, 'RELEASE'):
+            self.OCTO_SESSION.delete_release(self.RELEASE)
+        if hasattr(self, 'DEPLOYED_ENV'):
+            self.OCTO_SESSION.delete_environment(self.QUALI_ENV_SPEC)
 
     def test_create_new_environment(self):
-        self.OCTO_SESSION.create_machine(self.QUALI_ENV_SPEC)
+        self.DEPLOYED_ENV = self.OCTO_SESSION.create_machine(self.QUALI_ENV_SPEC)
         self.assertTrue(self.OCTO_SESSION.machine_exists(self.QUALI_ENV_SPEC))
 
     def test_create_machine(self):
-        env = self._given_an_environment_exists_on_octopus()
+        self.DEPLOYED_ENV = self._given_an_environment_exists_on_octopus()
         machine_spec = MachineSpec(name='machine_name',
                                    roles=['role1', 'role2'],
-                                   thumbprint='84B16EEA820A9D68523D56FCD0B9A290346E85D7',
+                                   thumbprint=THUMBPRINT,
                                    uri=TENTACLE_SERVER,
-                                   environment_ids=[env.id])
-        self.MACHINE = self.OCTO_SESSION.create_machine(machine_spec)
-        self.OCTO_SESSION.machine_exists(machine_spec)
+                                   environment_ids=[self.DEPLOYED_ENV.id])
+        self.MACHINES = [self.OCTO_SESSION.create_machine(machine_spec)]
+        self.assertTrue(self.OCTO_SESSION.machine_exists(self.MACHINES[0]))
 
-    def _given_an_environment_exists_on_octopus(self, octopus_session=None, env=None):
+    def test_create_release(self):
+        release_spec = ReleaseSpec(project_id=PROJECT_ID,
+                                   version='.'.join([str(randint(0, 8000000)),
+                                                     str(randint(0, 8000000)),
+                                                     str(randint(0, 8000000))]),
+                                   release_notes='Ya welli')
+        self.RELEASE = self.OCTO_SESSION.create_release(release_spec)
+        self.assertTrue(self.OCTO_SESSION.release_exists(self.RELEASE))
+
+    def test_deploy_release(self):
+        self.DEPLOYED_ENV = self._given_an_environment_exists_on_octopus(machine_specs=[self.MACHINE_SPEC])
+        self.RELEASE = self._given_a_release_exists_on_octopus()
+        result = self.OCTO_SESSION.deploy_release(self.RELEASE, self.DEPLOYED_ENV)
+        self.assertTrue(self.OCTO_SESSION.wait_for_deployment_to_complete(result))
+
+    def _given_an_environment_exists_on_octopus(self, octopus_session=None, env=None, machine_specs=[]):
         if not octopus_session:
             octopus_session = self.OCTO_SESSION
         if not env:
             env = self.QUALI_ENV_SPEC
-        return octopus_session.create_environment(env)
+        deployed_env = octopus_session.create_environment(env)
+        if machine_specs:
+            self.MACHINES = []
+        for machine_spec in machine_specs:
+            machine_spec.environment_ids.append(deployed_env.id)
+            self.MACHINES.append(self.OCTO_SESSION.create_machine(machine_spec))
+        return deployed_env
+
+    def _given_a_release_exists_on_octopus(self, octopus_session=None, release=None):
+        if not octopus_session:
+            octopus_session = self.OCTO_SESSION
+        if not release:
+            release = self.QUALI_RELEASE_SPEC
+        return octopus_session.create_release(release)
