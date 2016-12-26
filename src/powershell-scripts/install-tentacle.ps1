136cfec8afdfd86a7a42dcaf588861284c5aadc4
Nahum Timerman <ntimerman@gmail.com>
2016-12-26 10:03:43 +0200

    added registration to octopus server plus parameters to install script

--- src/powershell-scripts/install-tentacle.ps1
+++ src/powershell-scripts/install-tentacle.ps1
@@ -1,3 +1,9 @@
+Param(
+[string]$OCTOPUS_SERVER,
+[string]$API_KEY,
+[string]$ENVIRONMENT_NAME
+)
+
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Invoke-WebRequest -Uri http://octopusdeploy.com/downloads/latest/OctopusTentacle64 -OutFile c:\OctopusTentacle_x64.msi
 cd c:\
@@ -10,3 +16,6 @@ cd "C:\Program Files\Octopus Deploy\Tentacle"
 .\Tentacle.exe configure --instance "Tentacle" --app "C:\Octopus\Applications" --console
 .\Tentacle.exe configure --instance "Tentacle" --port "10933" --console
 
+netsh advfirewall firewall add rule "name=Octopus Deploy Tentacle" dir=in action=allow protocol=TCP localport=10933
+.\Tentacle.exe register-with --instance "Tentacle" --server "$OCTOPUS_SERVER" --apiKey="$API_KEY" --role "web-server" --environment "$ENVIRONMENT_NAME" --comms-style TentaclePassive --console
+.\Tentacle.exe service --instance "Tentacle" --install --start --console
