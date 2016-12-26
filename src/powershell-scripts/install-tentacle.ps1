bdc9aeacdfcf366cf85b0d2cbb70f55aadccf1f5
Nahum Timerman <ntimerman@gmail.com>
2016-12-27 00:28:09 +0200

    l

--- src/powershell-scripts/install-tentacle.ps1
+++ src/powershell-scripts/install-tentacle.ps1
@@ -4,11 +4,12 @@ Param(
 [string]$ENVIRONMENT_NAME
 )
 
+Start-Transcript -Path "c:\tentacle-transcript.txt"
+
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Invoke-WebRequest -Uri http://octopusdeploy.com/downloads/latest/OctopusTentacle64 -OutFile c:\OctopusTentacle_x64.msi
 cd c:\
 msiexec /i OctopusTentacle_x64.msi /quiet
-
 
 & 'C:\Program Files\Octopus Deploy\Tentacle\Tentacle.exe' create-instance --instance "Tentacle" --config "C:\Octopus\Tentacle\Tentacle.config" --console
 & 'C:\Program Files\Octopus Deploy\Tentacle\Tentacle.exe' new-certificate --instance "Tentacle" --console
@@ -19,3 +20,5 @@ msiexec /i OctopusTentacle_x64.msi /quiet
 netsh advfirewall firewall add rule "name=Octopus Deploy Tentacle" dir=in action=allow protocol=TCP localport=10933
 & 'C:\Program Files\Octopus Deploy\Tentacle\Tentacle.exe' register-with --instance "Tentacle" --server "$OCTOPUS_SERVER" --apiKey="$API_KEY" --role "web-server" --environment "$ENVIRONMENT_NAME" --comms-style TentaclePassive --console
 & 'C:\Program Files\Octopus Deploy\Tentacle\Tentacle.exe' service --instance "Tentacle" --install --start --console
+
+Stop-Transcript
